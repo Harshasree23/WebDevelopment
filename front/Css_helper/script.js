a2351e0568f6f2b6b991280bd56b878f36601866
Harsha2352 <134911610+Harsha2352@users.noreply.github.com>
2024-07-23 20:14:05 +0530

    moved all flex propeties to a function called flex_properties()

--- front/Css_helper/script.js
+++ front/Css_helper/script.js
@@ -50,91 +50,98 @@ const createContainer = (message,attributes,values,n) =>{
 
 
 
-// for justify-content
 
-// all values of justify-content
-const justify_content = [ "flex-start",	
-    "flex-end",	
-    "center",	
-    "space-between",	
-    "space-around",	
-    "space-evenly",	];
-
-// all values of flex-direction
-
-const flex_direction = [
-    "row",
-    "row-reverse",	
-    "column",	
-    "column-reverse"
-];
-
-// all values of align-items
-
-const align_items = [
-    "normal",	
-    "stretch",		
-    "center",	
-    "flex-start",	
-    "flex-end",		
-    "start",	
-    "end"	
-];
-
-// all values of align-content
-
-const align_content =[
-    "stretch",	
-    "center",	
-    "flex-start",	
-    "flex-end",	
-    "space-between",	
-    "space-around",	
-    "space-evenly"
-];
-
-// all values of flex wrap
-
-const flex_wrap = [
-    "nowrap",		
-    "wrap",	
-    "wrap-reverse"
-];
-
-//for justify content
-for(let x of justify_content)
+const flex_properties = () =>
 {
-    createContainer(`display:flex \n justify-content:${x}`,["display","justifyContent"],["flex",x],5);
+    // all values of justify-content
+    const justify_content = [ "flex-start",	
+        "flex-end",	
+        "center",	
+        "space-between",	
+        "space-around",	
+        "space-evenly",	];
+
+    // all values of flex-direction
+
+    const flex_direction = [
+        "row",
+        "row-reverse",	
+        "column",	
+        "column-reverse"
+    ];
+
+    // all values of align-items
+
+    const align_items = [
+        "normal",	
+        "stretch",		
+        "center",	
+        "flex-start",	
+        "flex-end",		
+        "start",	
+        "end"	
+    ];
+
+    // all values of align-content
+
+    const align_content =[
+        "stretch",	
+        "center",	
+        "flex-start",	
+        "flex-end",	
+        "space-between",	
+        "space-around",	
+        "space-evenly"
+    ];
+
+    // all values of flex wrap
+
+    const flex_wrap = [
+        "nowrap",		
+        "wrap",	
+        "wrap-reverse"
+    ];
+
+    //for justify content
+    for(let x of justify_content)
+    {
+        createContainer(`display:flex \n justify-content:${x}`,["display","justifyContent"],["flex",x],5);
+    }
+
+    // for flex-direction
+    for(let x of flex_direction)
+    {
+        const temp = createContainer(`display:flex \n flex-direction:${x}`,['display','flexDirection','textAlign','fontSize'],['flex',x,'center','2rem'],3);
+        const temp_output = temp.children[1];
+        let i=0;
+        for(let exp_div of temp_output.childNodes)
+            exp_div.innerText=i++;
+    }
+
+    //for align items
+    for(let x of align_items)
+    {
+        createContainer(`display:flex \n align-items:${x}` , ['display','alignItems'] , ['flex',x],5);
+    }
+
+    // for align content
+    for(let x of align_content)
+    {
+        createContainer( `dispaly:flex \n align-content:${x} \n flex-wrap:wrap` , ['display','alignContent','flexWrap'] , ['flex',x,'wrap'] , 3);
+    }
+
+    //for flex wrap
+    for(let x of flex_wrap)
+    {
+        const temp = createContainer( `display:flex \n flex-wrap:${x} `, ['display','flexWrap'] , ['flex',x] , 15 );
+        const temp_output = temp.children[1];
+        let i=0;
+        for(let exp_div of temp_output.childNodes)
+            exp_div.innerText=i++;
+    }
 }
 
-// for flex-direction
-for(let x of flex_direction)
-{
-    const temp = createContainer(`display:flex \n flex-direction:${x}`,['display','flexDirection','textAlign','fontSize'],['flex',x,'center','2rem'],3);
-    const temp_output = temp.children[1];
-    let i=0;
-    for(let exp_div of temp_output.childNodes)
-        exp_div.innerText=i++;
-}
 
-//for align items
-for(let x of align_items)
-{
-    createContainer(`display:flex \n align-items:${x}` , ['display','alignItems'] , ['flex',x],5);
-}
 
-// for align content
-for(let x of align_content)
-{
-    createContainer( `dispaly:flex \n align-content:${x} \n flex-wrap:wrap` , ['display','alignContent','flexWrap'] , ['flex',x,'wrap'] , 3);
-}
 
-//for flex wrap
-for(let x of flex_wrap)
-{
-    const temp = createContainer( `display:flex \n flex-wrap:${x} `, ['display','flexWrap'] , ['flex',x] , 15 );
-    const temp_output = temp.children[1];
-    let i=0;
-    for(let exp_div of temp_output.childNodes)
-        exp_div.innerText=i++;
-}
+flex_properties();
