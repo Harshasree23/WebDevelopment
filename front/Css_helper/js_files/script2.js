ee0720d54df90bf9e41483aa2102ccd8380d51da
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-07-28 18:18:00 +0530

    FRONT - CSS_HELPER -> added tools.html and script2.js with code to border radius generator

---
+++ front/Css_helper/js_files/script2.js
@@ -0,0 +1,30 @@
+const topLeft = document.getElementById('top-left');
+const topRight = document.getElementById('top-right');
+const bottomLeft = document.getElementById('bottom-left');
+const bottomRight = document.getElementById('bottom-right');
+
+
+const testDiv = document.getElementById('test');
+const codeText = document.getElementById('code');
+
+
+const handleRadius = (slider) =>
+{
+  const sliderId = slider.id;
+  const sliderValue = slider.value + "%";
+
+  // Update text input values (optional)
+  document.getElementById(sliderId + '-text').value = sliderValue;
+
+  // Construct the border-radius style string
+  const radiusStyle = `${topLeft.value}% ${topRight.value}% ${bottomRight.value}% ${bottomLeft.value}%`;
+
+  // Apply the border-radius style to the testDiv
+  testDiv.style.borderRadius = radiusStyle;
+  updatecode(radiusStyle);
+}
+
+
+const updatecode = () =>{
+    codeText.innerText = `border-radius : ${testDiv.style.borderRadius} ; `;
+}
