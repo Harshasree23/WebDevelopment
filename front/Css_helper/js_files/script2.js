961774e1aeb14eff72c63e636ea159958fb445ac
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-07-28 21:00:47 +0530

    FRONT - CSS_HELPER -> added function to take input from text and change border-radius

--- front/Css_helper/js_files/script2.js
+++ front/Css_helper/js_files/script2.js
@@ -11,7 +11,7 @@ const codeText = document.getElementById('code');
 const handleRadius = (slider) =>
 {
   const sliderId = slider.id;
-  const sliderValue = slider.value + "%";
+  const sliderValue = slider.value;
 
   // Update text input values (optional)
   document.getElementById(sliderId + '-text').value = sliderValue;
@@ -25,6 +25,19 @@ const handleRadius = (slider) =>
 }
 
 
+//  to get the code inside the code div 
 const updatecode = () =>{
     codeText.innerText = `border-radius : ${testDiv.style.borderRadius} ; `;
 }
+
+
+
+const handleInputText = (text) => {
+  const sliderId = text.id.replace("-text","");
+  document.getElementById(sliderId).value = text.value;
+  const radiusStyle = `${topLeft.value}% ${topRight.value}% ${bottomRight.value}% ${bottomLeft.value}%`;
+
+  // Apply the border-radius style to the testDiv
+  testDiv.style.borderRadius = radiusStyle;
+  updatecode(radiusStyle);
+} 
