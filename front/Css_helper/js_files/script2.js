79abd86620e3b8bd29c2d6faac5bf2d5679ce22d
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-07-28 22:53:19 +0530

    FRONT - CSS_HELPER -> added functions,css,html to handle borders ( top,left,right,bottom )

--- front/Css_helper/js_files/script2.js
+++ front/Css_helper/js_files/script2.js
@@ -27,7 +27,11 @@ const handleRadius = (slider) =>
 
 //  to get the code inside the code div 
 const updatecode = () =>{
-    codeText.innerText = `border-radius : ${testDiv.style.borderRadius} ; `;
+    codeText.innerText = `border-radius : ${testDiv.style.borderRadius} ;\n `;
+    codeText.innerText += `border-top : ${testDiv.style.borderTop} ;\n`;
+    codeText.innerText += `border-left : ${testDiv.style.borderLeft} ;\n`;
+    codeText.innerText += `border-bottom : ${testDiv.style.borderBottom} ;\n`;
+    codeText.innerText += `border-right : ${testDiv.style.borderRight} ;`;
 }
 
 
@@ -41,3 +45,90 @@ const handleInputText = (text) => {
   testDiv.style.borderRadius = radiusStyle;
   updatecode(radiusStyle);
 } 
+
+
+
+
+// 
+// 
+// BORDERS
+// 
+// 
+const borderTable = document.getElementById('border-table');
+
+// function for borders
+const createBorder = (label) => {
+  const html = `<tr>
+    <td><h3>${label}</h3></td>
+    <td>
+      <label for="${label}-border">Border</label>
+      <select id="${label}-border" onchange="handleBorder(this)">
+        <option value="none">No</option>
+        <option value="solid" selected>Yes</option>
+      </select>
+    </td>
+    <td>
+      <label for="${label}-type">Type</label>
+      <select id="${label}-type" onchange="handleBorder(this)">
+        <option value="solid" selected>Solid</option>
+        <option value="dotted">Dotted</option>
+        <option label="dashed" value="dashed"> Dashed </option>
+        <option value="solid" label="solid"> Solid </option>
+        <option value="double" label="double"> Double </option>
+        <option value="groove" label="groove"> Groove </option>
+        <option value="ridge" label="ridge"> Ridge </option>
+        <option value="inset" label="inset"> Inset </option>
+        <option value="outset" label="outset"> Outset </option>
+        <option value="none" label="none"> None </option>
+        <option value="hidden" label="hidden"> Hidden </option>
+        </select>
+    </td>
+    <td>
+      <label for="${label}-width">Width (rem)</label>
+      <input type="number" id="${label}-width" value="0.2" oninput="handleBorder(this)">
+    </td>
+    <td>
+      <label for="${label}-color">Color</label>
+      <input type="color" id="${label}-color" oninput="handleBorder(this)" value='#ffa550'>
+    </td>
+  </tr>`;
+  borderTable.innerHTML += html;
+};
+
+createBorder('top');
+createBorder('right');
+createBorder('bottom');
+createBorder('left');
+
+// handling borders input
+const handleBorder = (element) => {
+  let label='';
+  if(element.id.includes('top'))  label='top';
+  else if( element.id.includes('bottom') )  label='bottom';
+  else if( element.id.includes('right') ) label='right';
+  else label='left';
+  const border = document.getElementById(`${label}-border`).value;
+  const type = document.getElementById(`${label}-type`).value;
+  const width = document.getElementById(`${label}-width`).value;
+  const color = document.getElementById(`${label}-color`).value;
+
+  // console.log(border,type,width,color);
+
+  const style = `${width}rem ${type} ${color}`;
+
+  switch (label) {
+    case 'top':
+      testDiv.style.borderTop = border === 'none' ? 'none' : style;
+      break;
+    case 'right':
+      testDiv.style.borderRight = border === 'none' ? 'none' : style;
+      break;
+    case 'bottom':
+      testDiv.style.borderBottom = border === 'none' ? 'none' : style;
+      break;
+    case 'left':
+      testDiv.style.borderLeft = border === 'none' ? 'none' : style;
+      break;
+  }
+  updatecode();
+};
