642e059ab1a0cfd669b4ad21034df23243552d37
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-08-05 18:53:15 +0530

    FRONT - NAVBARS  -> deleted the folder planning to integrate it in CSS_HELPER

--- front/NavBar/js.js
+++
@@ -1,7 +0,0 @@
-function navBarHandle(){
-    let dis = document.getElementById('side-nav').style.display;
-    if( dis == 'block')
-        document.getElementById('side-nav').style.display = 'none';
-    else
-        document.getElementById('side-nav').style.display = 'block';
-}
