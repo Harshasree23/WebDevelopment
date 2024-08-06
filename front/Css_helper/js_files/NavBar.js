a74585d9393f6ae5cd73eea09b195b0099e5ed9c
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-08-06 19:31:13 +0530

    FRONT - CSS_HELPER  ->  added files for navbar but not updated

---
+++ front/Css_helper/js_files/NavBar.js
@@ -0,0 +1,51 @@
+const codes = [
+    `
+        <div class="nav-bar">
+            <div class="nav-items">Home</div>
+            <div class="nav-items">About</div>
+            <div class="nav-items">Contact Us</div>
+            <div class="nav-items">Retail</div>
+            <div class="nav-items">Detail</div>
+            <div class="nav-items">Login</div>
+        </div>
+    `,
+    `
+
+    `
+];
+
+const css = [
+    `
+
+    `,
+]
+
+export const createNavBar = (heading,output) => 
+{
+    const head = document.createElement('p');
+    head.innerText = heading;
+
+
+    const navDisplayDiv = document.createElement('div');
+    navDisplayDiv.classList.add('nav-bar-display');
+    navDisplayDiv.appendChild(head);
+
+    const navOutputDiv = document.createElement('div');
+    navOutputDiv.classList.add('nav-bar-output');
+    navOutputDiv.innerHTML = output;
+    navDisplayDiv.appendChild(navOutputDiv);
+
+
+    const navCodeDiv = document.createElement('div');
+    navCodeDiv.classList.add('nav-bar-code');
+    const code = document.createElement('code');
+    code.innerText = output;
+    navCodeDiv.appendChild(code);
+    navDisplayDiv.appendChild(navCodeDiv);
+
+}
+
+const displayNavBar = () => 
+{
+
+}
