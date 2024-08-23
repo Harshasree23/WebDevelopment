d0a98e1bac141b87d7761c15c1ba167981b51de5
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-08-23 14:57:15 +0530

    FULL_PROJECT - MY_SERVER  -> connected to monog atlas cloud

--- Full_project/My_Server/connectToDb.js
+++ Full_project/My_Server/connectToDb.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const {db_password} = require('./config.json');
+
 
 const makeConnection  = async (db_name) => {
-    return await mongoose.connect(`mongodb://localhost:27017/${db_name}`);
+    return await mongoose.connect(`mongodb+srv://marvelavengersharsha:${db_password}@cluster0.xv2jk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/${db_name}`);
 }
 
 module.exports = {
