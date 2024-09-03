b8fc3e0ae6bcf6bdf9727cbae5968fd970f3c035
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-09-03 21:29:29 +0530

    FULL_PROJECT - MY_SERVER -> added favicon

--- Full_project/My_Server/index.js
+++ Full_project/My_Server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { apiRoute } = require('./routes/api');
 const { userRoute } = require('./routes/user');
 const { makeConnection } = require('./connectToDb.js');
-
+const favicon = require('express-favicon');
 
 // Creating app
 const app = express();
@@ -19,6 +19,7 @@ app.set('view engine','ejs');
 app.set('views',path.resolve('./views'));
 app.use(express.urlencoded( { extended : false } ));
 app.use(express.json());
+app.use("/public", express.static('public')); 
 
 
 // Routes
